3cdcdad6ba6c1c5b2c718bf14fa2caebbbe9fa34
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-04 12:40:17 +0100

    Add project files.

---
+++ CSTaroltEljarasKonstansGenerator/Generator.cs
@@ -0,0 +1,86 @@
+﻿using d7p4n4Namespace.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSTaroltEljarasKonstansGenerator
+{
+    class Generator
+    {
+        public static void generateClass(string outputPath, TaroltEljaras arguments, TaroltEljaras methods)
+        {
+            StringToPascalCase converter = new StringToPascalCase();
+
+            string[] text = readIn("TemplateTaroltEljarasMuvelet");
+
+            string replaced = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains("enums"))
+                {
+                    int x = 0;
+                    string newline = "";
+                    foreach (var muvelet in methods.MuveletLista)
+                    {
+                        newline = newline + text[i + 1].Replace("#muveletNev#", converter.Convert(muvelet.Leiras)).Replace("#num#", "" + x) + "\n";
+                        Console.WriteLine(newline);
+                        x++;
+
+                    }
+                    replaced = replaced + newline + "\n";
+                    i = i + 2;
+                }
+                else if (text[i].Contains("argumentumKonstans"))
+                {
+                    string newline = "";
+                    foreach (var arg in arguments.ArgumentumLista)
+                    {
+                        if (!arg.BelsoNev.Equals("@RETURN_VALUE"))
+                        {
+                            newline = newline + text[i + 1].Replace("#@argNev#", arg.BelsoNev).Replace("#argNev#", arg.BelsoNev.Substring(1)) + "\n";
+                            Console.WriteLine(newline);
+                        }
+                    }
+                    replaced = replaced + newline + "\n";
+                    i = i + 2;
+                }
+                else if (text[i].Contains("muveletKonstans"))
+                {
+                    string newline = "";
+                    foreach (var muvelet in methods.MuveletLista)
+                    {
+                        newline = newline + text[i + 1].Replace("#muveletNev#", converter.Convert(muvelet.Leiras)).Replace("#muveletKod#", muvelet.Kod) + "\n";
+                        Console.WriteLine(newline);
+
+                    }
+                    replaced = replaced + newline + "\n";
+                    i = i + 2;
+                }
+                replaced = replaced + text[i] + "\n";
+            }
+
+            replaced = replaced.Replace("#taroltEljarasNev#", methods.Kod);
+
+            writeOut(replaced, methods.Kod, outputPath);
+
+        }
+
+        public static string[] readIn(string fileName)
+        {
+
+            string textFile = "c:\\Templates\\c#\\Tabla\\" + fileName + ".csT";
+
+            string[] text = File.ReadAllLines(textFile);
+
+            return text;
+        }
+
+        public static void writeOut(string text, string fileName, string outputPath)
+        {
+            File.WriteAllText(outputPath + fileName + ".cs", text);
+
+        }
+    }
+}
---
+++ CSTaroltEljarasKonstansGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using d7p4n4Namespace.Final.Class;
+using System;
+using System.IO;
+
+namespace CSTaroltEljarasKonstansGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deserializer deserializer = new Deserializer();
+
+            try
+            {
+                string[] filesMethods =
+                    Directory.GetFiles("d:\\Server\\Visual_studio\\output_Xmls\\StorProcsMethods\\", "*.xml", SearchOption.TopDirectoryOnly);
+
+                string[] filesArgs =
+                    Directory.GetFiles("d:\\Server\\Visual_studio\\output_Xmls\\StorProcs\\", "*.xml", SearchOption.TopDirectoryOnly);
+
+                Console.WriteLine(filesMethods.Length);
+
+                foreach (var _file in filesArgs)
+                {
+                    TaroltEljaras ujTaroltEljaras = new TaroltEljaras();
+                    TaroltEljaras ujTaroltEljarasMethods = new TaroltEljaras();
+                    string _filename = Path.GetFileNameWithoutExtension(_file);
+                    Console.WriteLine(_filename);
+                    foreach(var fileMethod in filesMethods)
+                    {
+                        string muvFileName = Path.GetFileNameWithoutExtension(fileMethod);
+                        string muvelet = muvFileName.Substring(0, muvFileName.IndexOf("@"));
+                        if (_filename.ToLower().Equals(muvelet.ToLower()))
+                        {
+                            Console.WriteLine(">>PAIR: " + _filename + " " + muvelet);
+                            string textFileArgs = Path.Combine("d:\\Server\\Visual_studio\\output_Xmls\\StorProcs\\", _filename + ".xml");
+                            string textFileMethods = Path.Combine("d:\\Server\\Visual_studio\\output_Xmls\\StorProcsMethods\\", muvFileName + ".xml");
+
+                            string textArgs = File.ReadAllText(textFileArgs);
+                            string textMethods = File.ReadAllText(textFileMethods);
+
+                            ujTaroltEljaras = deserializer.deser(textArgs);
+                            ujTaroltEljarasMethods = deserializer.deser(textMethods);
+                            Generator.generateClass("d:\\Server\\Visual_studio\\AK\\outputTaroltEljaras\\", ujTaroltEljaras, ujTaroltEljarasMethods);
+                        }
+
+                    }
+                    
+
+                }
+            }
+            catch (Exception _exception)
+            {
+                Console.WriteLine(_exception.StackTrace);
+            }
+        }
+    }
+}
+
